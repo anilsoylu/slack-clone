039060c832fb5616a46682c4c4b9d9df5de1c58a
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2024-09-16 03:40:45 +0300

    feat:auth screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from "@/components/ui/button"
-import React from "react"
+import { AuthScreen } from "@/feautres/auth/components/auth-screen"
 
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <Button>Click me</Button>
-    </div>
-  )
+  return <AuthScreen />
 }
